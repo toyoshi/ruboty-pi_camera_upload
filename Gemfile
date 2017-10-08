a01d50eafe2af487c90f3f252e7efaa40c77ca83
toyoshi <toyosystem@gmail.com>
2017-10-09 01:39:13 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in ruboty-pi_camera_upload.gemspec
+gemspec
---
+++ lib/ruboty/pi_camera_upload.rb
@@ -0,0 +1,7 @@
+require "ruboty/pi_camera_upload/version"
+
+module Ruboty
+  module PiCameraUpload
+    # Your code goes here...
+  end
+end
