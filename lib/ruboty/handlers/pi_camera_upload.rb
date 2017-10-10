d9624fcbfdd767f129b13baa433f17ec9a264dba
toyoshi <toyosystem@gmail.com>
2017-10-10 23:56:15 +0900

    写真を撮ってアップロードするようにした

--- lib/ruboty/handlers/pi_camera_upload.rb
+++ lib/ruboty/handlers/pi_camera_upload.rb
@@ -1,20 +1,26 @@
 require 'slack'
 require 'faraday'
+require 'tmpdir'
 
 module Ruboty
   module Handlers
     class PiCameraUpload < Base
-      on(/camera/i, name: 'pi_camera_upload', description: "ラズパイの写真をアップロードします。")
+      on(/take photo|ワンタイムトークン/i, name: 'pi_camera_upload', description: "ラズパイの写真をアップロードします。")
 
       def pi_camera_upload(message)
         Slack.configure do |config|
           config.token = ENV['SLACK_TOKEN']
         end
 
-        Slack.chat_postMessage(
-          channel: '#current_toyoshi_dev',
-          text: 'test'
-        )
+        Dir.mktmpdir do |dir|
+          file_name = "#{dir}/image.png"
+          system(`raspistill -w 480 -h 360 -o #{file_name}`)
+          Slack.files_upload(
+            file: Faraday::UploadIO.new(file_name, 'image/png'),
+            channels: "##{ENV['PI_CAMERA_UPLOAD_CHANNEL']}",
+            initial_comment: ''
+          )
+        end
       end
     end
   end
