4b66e807f618be909176a117f2a1b0c9ecac5ee6
toyoshi <toyosystem@gmail.com>
2017-10-13 15:42:59 +0900

    メソッドを分割するリファクタリングをした

--- lib/ruboty/handlers/pi_camera_upload.rb
+++ lib/ruboty/handlers/pi_camera_upload.rb
@@ -5,17 +5,14 @@ require 'tmpdir'
 module Ruboty
   module Handlers
     class PiCameraUpload < Base
-      on(/take photo|ワンタイムトークン/i, name: 'pi_camera_upload', description: "ラズパイの写真をアップロードします。")
+      on(/take photo|ワンタイムトークン/i, name: 'pi_camera_upload', description: 'ラズパイの写真をアップロードします。')
 
       def pi_camera_upload(message)
         message.reply('撮影してくるから、ちょっと待ってね')
 
-        Slack.configure do |config|
-          config.token = ENV['SLACK_TOKEN']
-        end
-p message
         Dir.mktmpdir do |dir|
           file_name = "#{dir}/image.png"
+          
           begin
             system(`raspistill -t 0 -w 480 -h 360 -o #{file_name}`)
           rescue
@@ -23,13 +20,21 @@ p message
             return
           end
 
-          Slack.files_upload(
-            filename: 'Raspberry Pi Camera Image.png',
-            file: Faraday::UploadIO.new(file_name, 'image/png'),
-            channels: "##{ENV['PI_CAMERA_UPLOAD_CHANNEL']}",
-            initial_comment: ''
-          )
+          upload_photo(file_name)
         end
+      end
+
+      def upload_photo(file_name)
+        Slack.configure do |config|
+          config.token = ENV['SLACK_TOKEN']
+        end
+
+        Slack.files_upload(
+          filename: 'Raspberry Pi Camera Image.png',
+          file: Faraday::UploadIO.new(file_name, 'image/png'),
+          channels: "##{ENV['PI_CAMERA_UPLOAD_CHANNEL']}",
+          initial_comment: ''
+        )
       end
     end
   end
